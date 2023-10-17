1657ae00e042c260a0c4b0a3f66cab336aca1e88
shehroz665 <shehrozmalik665@gmail.com>
2023-10-17 19:12:39 +0500

    made all views table

--- src/app/category/viewcategory/viewcategory.component.ts
+++ src/app/category/viewcategory/viewcategory.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./viewcategory.component.css']
 })
 export class ViewcategoryComponent {
-
+  data:any[]= [
+    {
+      name: 'Russia',
+      area: 17075200,
+      population: 146989754,
+    },
+    {
+      name: 'Canada',
+      area: 9976140,
+      population: 36624199,
+    },
+    {
+      name: 'United States',
+      area: 9629091,
+      population: 324459463,
+    },
+    {
+      name: 'China',
+      area: 9596960,
+      population: 1409517397,
+    },
+    {
+      name: 'Russia',
+      area: 17075200,
+      population: 146989754,
+    },
+    {
+      name: 'Canada',
+      area: 9976140,
+      population: 36624199,
+    },
+    {
+      name: 'United States',
+      area: 9629091,
+      population: 324459463,
+    },
+    {
+      name: 'China',
+      area: 9596960,
+      population: 1409517397,
+    },
+  ];
 }
