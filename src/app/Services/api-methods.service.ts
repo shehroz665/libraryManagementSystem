aa707a0e0d827102c0ab24d64e896d4cb5d2baa3
shehroz665 <shehrozmalik665@gmail.com>
2023-10-18 14:42:32 +0500

    sweet alerts,post data in authors

--- src/app/Services/api-methods.service.ts
+++ src/app/Services/api-methods.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
-
+import { HttpClient } from '@angular/common/http'
+import Swal from 'sweetalert2'
 @Injectable({
   providedIn: 'root'
 })
 export class ApiMethodsService {
 
-  constructor(private http:HttpClient) { }
-  getDataFromApi(url:string){
-   return  this.http.get(url);
+  constructor(private http: HttpClient) { }
+  getDataFromApi(url: string) {
+    return this.http.get(url);
+  }
+  postDataUsingApi(url: string, data: any) {
+    return this.http.post(url, data);
+  }
+  successAlert(message: string) {
+    return Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: message,
+      showConfirmButton: false,
+      timer: 1500
+    });
+  }
+  errorAlert(message: string) {
+    return Swal.fire({
+      icon: 'error',
+      text: message
+    });
   }
 }
--- src/app/author/add/add.component.ts
+++ src/app/author/add/add.component.ts
@@ -1,29 +1,51 @@
 import { Component } from '@angular/core';
-import { FormGroup,FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import Swal from 'sweetalert2'
+import { ApiMethodsService } from 'src/app/Services/api-methods.service';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
 export class AddComponent {
-  constructor(private router:Router){}
-  authorFormData=new FormGroup({
-    AuthName:new FormControl('',[Validators.required]),
+  constructor(private router: Router, private api: ApiMethodsService) { }
+  url: string = 'https://localhost:7084/api/author';
+  authorFormData = new FormGroup({
+    AuthName: new FormControl('', [Validators.required]),
   });
-  get authName(){
-   return  this.authorFormData.get('AuthName');
+  get authName() {
+    return this.authorFormData.get('AuthName');
   }
-  goToView(){
-    if(this.authorFormData.value.AuthName!==''){
-      console.log('filled');
-      this.router.navigate(['/author/view']);
+  postData() {
+    if (this.authorFormData.value.AuthName !== '') {
+      var data = {
+        AuthName: this.authorFormData.value.AuthName,
+        Status: 1
+      };
+      console.log(data);
+      this.api.postDataUsingApi(this.url, data).subscribe((response: any) => {
+        console.log(response);
+        if (response.statuscode === 201) {
+          this.api.successAlert(response.message);
+          this.authorFormData.reset();
+          this.router.navigate(['/author/view']);
+        }
+        else {
+          this.api.errorAlert(response.message);
+        }
+
+      })
+
     }
-    else{
-      console.log('not filled');
+    else {
+      // Swal.fire({
+      //   icon: 'error',
+      //   text: 'Please fill the forms fields...',
+      // });
+      this.api.errorAlert('Please fill the forms fields...');
     }
-  
+
   }
 
 }
