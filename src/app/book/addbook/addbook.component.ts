cd53a641ed03eb3e4bef7f38336298c9ffd9f6bd
shehroz665 <shehrozmalik665@gmail.com>
2023-10-19 13:21:48 +0500

    book added using api

--- src/app/book/addbook/addbook.component.ts
+++ src/app/book/addbook/addbook.component.ts
@@ -16,46 +16,44 @@ export class AddbookComponent {
   constructor(private router: Router, private api: ApiMethodsService) {
     this.api.getDataFromApi('https://localhost:7084/api/generic/dropdown').subscribe((response:any)=> {
         this.authors= response.data.authors;
-        this.categories=response.data.category;   
+        this.categories=response.data.category; 
+        console.log(response.data);
+          
     })
    }
   bookFormData = new FormGroup({
     Title:new FormControl('aa', [Validators.required]),
-    CatName:new FormControl('', [Validators.required]),
-    AutName:new FormControl('', [Validators.required]),
-    Quanity:new FormControl(10, [Validators.required]),
-    Price:new FormControl(80, [Validators.required]), 
-    ActualQuantity:new FormControl(80, [Validators.required]),
-    AvailableQuantity:new FormControl(80, [Validators.required]),  
-    Isbn:new FormControl(1234567890123, [Validators.required,Validators.pattern(/^\d{13}$/)]),   
+    BookCatId:new FormControl('', [Validators.required]),
+    BookAuthId:new FormControl('', [Validators.required]),
+    Quanity:new FormControl(13, [Validators.required]),
+    Price:new FormControl(29, [Validators.required]), 
+    ActualQuantity:new FormControl(27, [Validators.required]),
+    AvailableQuantity:new FormControl(29, [Validators.required]),  
+    Isbn:new FormControl('1122334455113', [Validators.required,Validators.pattern(/^\d{13}$/)]),   
   });
+
   getControl(controlName:string) {
     return this.bookFormData.get(controlName);
   }
   postData() {
-    console.log(this.bookFormData.value);
-    
-    // if (this.authorFormData.value.Title !== '') {
-    //   var data = {
-    //     AuthName: this.authorFormData.value.Title,
-    //     Status: 1
-    //   };
-    //   console.log(data);
-    //   this.api.postDataUsingApi(this.url, data).subscribe((response: any) => {
-    //     console.log(response);
-    //     if (response.statuscode === 201) {
-    //       this.api.successAlert(response.message);
-    //       this.authorFormData.reset();
-    //       this.router.navigate(['/author/view']);
-    //     }
-    //     else {
-    //       this.api.errorAlert(response.message);
-    //     }
-    //   })
-    // }
-    // else {
-    //   this.api.errorAlert('Please fill the forms fields...');
-    // }
-
+    var book =this.bookFormData.value;
+    var data={
+      Title:book.Title,
+      BookCatId:Number(book.BookCatId),
+      BookAuthId:Number(book.BookAuthId),
+      Price:book.Price, 
+      ActualQuantity:book.ActualQuantity,
+      AvailableQuantity:book.AvailableQuantity,  
+      Isbn:book.Isbn, 
+    };
+    this.api.postDataUsingApi('https://localhost:7084/api/books',data).subscribe((response:any)=> {
+      if(response.statuscode===201){
+        this.api.successAlert(response.message);
+        this.router.navigate(['/book/view']);
+      }
+      else{
+        this.api.errorAlert(response.message);
+      }
+    });
   }
 }
