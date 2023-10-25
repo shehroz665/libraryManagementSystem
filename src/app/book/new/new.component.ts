2701c51bcefdfc8b560c9d696badf3e42a2150cb
shehroz665 <shehrozmalik665@gmail.com>
2023-10-25 14:20:48 +0500

    apply for book form handled

--- src/app/book/new/new.component.ts
+++ src/app/book/new/new.component.ts
@@ -2,48 +2,53 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiMethodsService } from 'src/app/Services/api-methods.service';
-import { NgbDatepicker } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
   styleUrls: ['./new.component.css']
 })
 export class NewComponent {
-  constructor(private router: Router, private api: ApiMethodsService) { }
-  url: string = 'https://localhost:7084/api/author';
+  constructor(private router: Router, private api: ApiMethodsService) {
+    this.api.getDataFromApi('https://localhost:7084/api/books/dropdown').subscribe((response: any) => {
+      this.books = response.data;
+      console.log(response.data);
+    })
+  }
+  userId: number = Number(this.api.getTokenFields('UserId'));
+  books: any[] = [];
   applyBookForm = new FormGroup({
     book: new FormControl('', [Validators.required]),
-    borrowDate:new FormControl('', [Validators.required]),
-    dueDate:new FormControl('', [Validators.required]),
-    returnDate:new FormControl('', [Validators.required]),
+    borrowDate: new FormControl('', [Validators.required]),
+    dueDate: new FormControl('', [Validators.required]),
+    // returnDate:new FormControl('', [Validators.required]),
   });
-  getControl(controlName:string) {
+  getControl(controlName: string) {
     return this.applyBookForm.get(controlName);
   }
   postData() {
-    console.log(this.applyBookForm.value);
-    
-    // if (this.applyBookForm.value.AuthName !== '') {
-    //   var data = {
-    //     AuthName: this.applyBookForm.value.AuthName,
-    //     Status: 1
-    //   };
-    //   console.log(data);
-    //   this.api.postDataUsingApi(this.url, data).subscribe((response: any) => {
-    //     console.log(response);
-    //     if (response.statuscode === 201) {
-    //       this.api.successAlert(response.message);
-    //       this.applyBookForm.reset();
-    //       this.router.navigate(['home/author/view']);
-    //     }
-    //     else {
-    //       this.api.errorAlert(response.message);
-    //     }
-    //   })
-    // }
-    // else {
-    //   this.api.errorAlert('Please fill the forms fields...');
-    // }
+    const formData = this.applyBookForm.value;
+    const data = {
+      TransStuId: 0,
+      TransBookId: Number(formData.book),
+      BorrowedDate: formData.borrowDate,
+      DueDate: formData.dueDate,
+      ReturnedDate: '',
+      Status: 1,
+      UserId: this.userId,
+    }
+      this.api.postDataUsingApi('https://localhost:7084/api/transaction', data).subscribe((response: any) => {
+      console.log(response);
+      if (response.statuscode === 201) {
+        this.api.successAlert(response.message);
+        this.applyBookForm.reset();
+        this.router.navigate(['home/book/apply']);
+      }
+      else {
+        this.api.errorAlert(response.message);
+      }
+    })
+
+
 
   }
 }
